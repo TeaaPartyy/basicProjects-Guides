5504b0921753c56d32076e08b38784e2be7b43fd
Hamza Slaoui Habib <58532640+TeaaPartyy@users.noreply.github.com>
2021-03-23 13:25:37 +0100

    Update footer.php

--- netflix_sim/src/footer.php
+++ netflix_sim/src/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <p>Simulation of Netflix Created by Hamza Slaoui Habib.</p>
         <p>The creator of this project does not possess any right for the image/brand displayed here.This project has not been created for any economical purposes, only for learning.</p>
-        <a href="https://www.linkedin.com/in/slaouihabibhamza/">Contact</a> <a href="https://github.com/TeaaPartyy/basicProjectsAndGuides">Source Code</a>
+        <a href="https://www.linkedin.com/in/slaouihabibhamza/">Contact</a> <a href="https://github.com/TeaaPartyy/basicProjectsAndGuides/tree/main/netflix_sim">Source Code</a>
     </div>
 
 </footer>
